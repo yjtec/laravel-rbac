4cf5f1ac192e4778cfca6afc12c6d65f7d4bdf44
kidkang <723616880@qq.com>
2019-08-08 04:41:49 +0800

    delete migrations file

--- src/Providers/RbacServiceProvider.php
+++ src/Providers/RbacServiceProvider.php
@@ -13,7 +13,7 @@ class RbacServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        //$this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         $this->publishes([
             __DIR__ . '/../database/migrations/' => database_path('migrations'),
