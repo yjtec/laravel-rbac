1fe87a0948a271c039fa1ef3f4fcb35807dded6c
playhan <2824643212@qq.com>
2020-07-18 09:29:09 +0800

    修改数据库配置引入

--- src/Models/Model.php
+++ src/Models/Model.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Model as BaseModel;
 
 class Model extends BaseModel
 {
-    // public function __construct(){
-    //     $this->setConnection(config('rbac.connection','rbac'));
-    // }
+    public function __construct(){
+        $this->setConnection(config('rbac.connection','rbac'));
+    }
     //protected $connection = config('rbac.connection','rbac');
 }
